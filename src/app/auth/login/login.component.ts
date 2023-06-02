84e86d86bf4838626639860d6640b097c4f46291
James Bowers <jamesbowers123@gmail.com>
2023-06-02 15:37:48 +0100

    define ngrx actions and group together with action types

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,9 +4,11 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Store} from "@ngrx/store";
 
 import {AuthService} from "../auth.service";
+import {login} from "../auth.actions";
 import {tap} from "rxjs/operators";
 import {noop} from "rxjs";
 import {Router} from "@angular/router";
+import { AppState } from '../reducers';
 
 @Component({
   selector: 'login',
@@ -20,7 +22,8 @@ export class LoginComponent implements OnInit {
   constructor(
       private fb:FormBuilder,
       private auth: AuthService,
-      private router:Router) {
+      private router:Router,
+      private store: Store<AppState>) {
 
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
@@ -34,6 +37,24 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+
+    const val = this.form.value;
+
+    this.auth.login(val.email, val.password)
+      .pipe(
+        tap(user => {
+
+          console.log(user);
+
+          this.store.dispatch(login({user}));
+
+          this.router.navigateByUrl('/courses');
+        })
+      )
+      .subscribe(
+        noop,
+        () => alert('Login Failed')
+      );
 
   }
 
